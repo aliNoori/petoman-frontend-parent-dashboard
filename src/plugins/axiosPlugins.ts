642349b35d974f6e99720cc3b34f9d2e87ddd114
Matin  Noori <matineali@gmail.com>
2026-07-05 11:35:44 +0330

    commit for .env

--- src/plugins/axiosPlugins.ts
+++ src/plugins/axiosPlugins.ts
@@ -8,7 +8,7 @@ export const axiosKey = Symbol('axios') // ✅ کلید یکتا برای inject
 export default {
     install(app: App) {
         const instance = axios.create({
-            baseURL: 'http://127.0.0.1:3000/api',
+            baseURL:import.meta.env.VITE_API_URL,
             //baseURL: 'https://api.petoman.com/api',
         })
 
